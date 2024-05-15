a75bea1f38ba2f1068b7f0879f1115800e8e5445
Umang Chaudhary <uchaudhary2022002@gmail.com>
2024-05-15 23:24:19 +0530

    design changes

--- migrations/config.py
+++ migrations/config.py
@@ -1,11 +1,13 @@
 # config.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 app = Flask(__name__, template_folder="../templates",static_folder="../static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOADED_PDFS_DEST'] = 'static/pdfs'
+if not os.path.exists(app.config['UPLOADED_PDFS_DEST']):
+    os.makedirs(app.config['UPLOADED_PDFS_DEST'])
 app.config['SECRET_KEY'] = 'secret'
 
 db = SQLAlchemy(app)
